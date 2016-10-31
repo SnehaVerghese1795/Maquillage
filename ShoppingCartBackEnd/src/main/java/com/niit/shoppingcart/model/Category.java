cf97164d5ae30d0efe177a46f69ae91f925bee5e
SnehaVerghese1795 <sneha05verghese@gmail.com>
2016-10-31 17:53:15 +0530

    this is my sixth commit

---
+++ ShoppingCartBackEnd/src/main/java/com/niit/shoppingcart/model/Category.java
@@ -0,0 +1,29 @@
+package com.niit.shoppingcart.model;
+
+import org.springframework.stereotype.Component;
+
+@Component("C1")//creates the object of category
+public class Category {
+	private String id;
+	private String name;
+	private String description;
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
+}
